045c8402340ac36be2b876c1a41b05b7da633c70
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-17 02:35:53 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+  string_array = string.split(/ /)
+  reversed = []
+  string_array.collect do |memo, i| 
+    
+    
+    
+  end 
+end
+reverse_each_word("Hello there, and how are you?")
